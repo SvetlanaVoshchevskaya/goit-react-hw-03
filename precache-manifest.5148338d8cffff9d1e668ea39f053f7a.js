4ec65cbc32d0e4ca212404569e62cdecb4ec75ce
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-07-06 22:15:17 +0300

    Updates

---
+++ precache-manifest.5148338d8cffff9d1e668ea39f053f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a103ddc6d817311e2ffc2ab594416942",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "41282f78a65b5c8a50d1",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.10583f31.chunk.css"
+  },
+  {
+    "revision": "02a0a4d533d180c11fea",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.47a4158d.chunk.css"
+  },
+  {
+    "revision": "41282f78a65b5c8a50d1",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.44ba2d9d.chunk.js"
+  },
+  {
+    "revision": "02a0a4d533d180c11fea",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.0ed9fe3c.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
